8c5bd16afda2d7e0e2243865f920794e0c438bec
jorisamarin <bgj416@myy.haaga-helia.fi>
2021-05-18 12:27:36 +0300

    addkysely

--- src/main/java/fi/hh/ohjelmistoprojekti/backend2/WebSecurityConfig.java
+++ src/main/java/fi/hh/ohjelmistoprojekti/backend2/WebSecurityConfig.java
@@ -25,7 +25,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-        .authorizeRequests().antMatchers("/css/**", "/kysymykset/{nimi}", "/kysely", "/kysely/{id}", "/addkysymys", "/api/vastauses", "/kysymykset/{nimi}/vastaus").permitAll() // Enable css when logged out
+        .authorizeRequests().antMatchers("/css/**", "/kysymykset/{nimi}", "/kysely", "/kysely/{id}", "/addKysymys", "/api/vastauses", "/kysymykset/{nimi}/vastaus", "addkysely").permitAll() // Enable css when logged out
         .and()
         .authorizeRequests().anyRequest().authenticated()
         .and()
